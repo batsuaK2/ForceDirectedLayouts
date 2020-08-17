530ce2819dbc85938a79051c74e3ce55fa325b97
batsuaK <kbasu.irl@gmail.com>
2020-08-17 23:47:34 +0100

    Update barycentric.jl

--- src/barycentric.jl
+++ src/barycentric.jl
@@ -21,46 +21,37 @@ function barycentric_layout(g::AbstractGraph,
            force_x = zeros(nvg)
            force_y = zeros(nvg)
 
-           c1 = 2
-           c2 = 1
-           c3 = 1
-           c4 = 0.1
+           for i = 1:nvg
+               force_vec_x = 0.0
+               force_vec_y = 0.0
+               neibr = 0
+               for j = 1:nvg
+                   i == j && continue
+                   d_x = locs_x[j] - locs_x[i]
+                   d_y = locs_y[j] - locs_y[i]
+                   dist²  = (d_x * d_x) + (d_y * d_y)
+                   dist = sqrt(dist²)
 
-           # Iterate MAXITER times
-           # @inbounds for iter = 1:MAXITER
-               # Calculate forces
-               for i = 1:nvg
-                   force_vec_x = 0.0
-                   force_vec_y = 0.0
-                   neibr = 0
-                   for j = 1:nvg
-                       i == j && continue
-                       d_x = locs_x[j] - locs_x[i]
-                       d_y = locs_y[j] - locs_y[i]
-                       dist²  = (d_x * d_x) + (d_y * d_y)
-                       dist = sqrt(dist²)
-
-                       if !( iszero(adj_matrix[i,j]) && iszero(adj_matrix[j,i]) )
-                           neibr += 1
-                           force_vec_x += d_x
-                           force_vec_y += d_y
-                       end
+                   if !( iszero(adj_matrix[i,j]) && iszero(adj_matrix[j,i]) )
+                       neibr += 1
+                       force_vec_x += d_x
+                       force_vec_y += d_y
                    end
-                   force_x[i] = force_vec_x * (1/neibr)
-                   force_y[i] = force_vec_y * (1/neibr)
                end
+               force_x[i] = force_vec_x * (1/neibr)
+               force_y[i] = force_vec_y * (1/neibr)
+           end
                # Cool down
-               iter = 1
-               temp = INITTEMP / iter
+           iter = 1
+           temp = INITTEMP / iter
                # Now apply them, but limit to temperature
-               for i = 1:nvg
-                   fx = force_x[i]
-                   fy = force_y[i]
-                   force_mag  = sqrt((fx * fx) + (fy * fy))
-                   scale      = min(force_mag, temp) / force_mag
-                   work_x[i] += force_x[i] * scale
-                   work_y[i] += force_y[i] * scale
-               end
+           for i = 1:nvg
+               fx = force_x[i]
+               fy = force_y[i]
+               force_mag  = sqrt((fx * fx) + (fy * fy))
+               scale      = min(force_mag, temp) / force_mag
+               work_x[i] += force_x[i] * scale
+               work_y[i] += force_y[i] * scale
            end
 
            # Scale to unit square
